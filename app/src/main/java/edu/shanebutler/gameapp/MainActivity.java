1682fc9806eb293208ca5b63a86ad01bd63ae228
ShaneB22 <47157867+ShaneB22@users.noreply.github.com>
2020-12-15 21:40:35 +0000

    Four buttons change colour

--- app/src/main/java/edu/shanebutler/gameapp/MainActivity.java
+++ app/src/main/java/edu/shanebutler/gameapp/MainActivity.java
@@ -4,16 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.ViewCompat;
 
 import android.content.res.ColorStateList;
-import android.content.res.TypedArray;
-import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnTest;
+    Button btnOne;
+    Button btnTwo;
+    Button btnThree;
+    Button btnFour;
 
 
     @Override
@@ -21,16 +23,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnTest = findViewById(R.id.btnTest);
-
-
-       // String[] array = getResources().getStringArray(R.array.randomColours);
-
+        btnOne = findViewById(R.id.btnOne);
+        btnTwo = findViewById(R.id.btnTwo);
+        btnThree = findViewById(R.id.btnThree);
+        btnFour = findViewById(R.id.btnFour);
 
 
         //How to set a button's background colour to random colour from array.
-        int[] randomColours = getResources().getIntArray(R.array.randomColours);
-        ViewCompat.setBackgroundTintList(btnTest, ColorStateList.valueOf(randomColours[new Random().nextInt(randomColours.length)]));
+        AssignRandomColour(btnOne);
+        AssignRandomColour(btnTwo);
+        AssignRandomColour(btnThree);
+        AssignRandomColour(btnFour);
 
+    }
+
+    private void AssignRandomColour(Button button)
+    {
+        int[] randomColours = getResources().getIntArray(R.array.randomColours);
+        ViewCompat.setBackgroundTintList(button, ColorStateList.valueOf( randomColours[new Random().nextInt(randomColours.length)]));
     }
 }
