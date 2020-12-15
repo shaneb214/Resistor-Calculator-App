6ff13e6a5778177cd79e8220f602eb3717cd7f2a
ShaneB22 <47157867+ShaneB22@users.noreply.github.com>
2020-12-15 20:56:41 +0000

    Added colour xml file

--- app/src/main/java/edu/shanebutler/gameapp/MainActivity.java
+++ app/src/main/java/edu/shanebutler/gameapp/MainActivity.java
@@ -1,14 +1,36 @@
 package edu.shanebutler.gameapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
 
+import android.content.res.ColorStateList;
+import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnTest;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnTest = findViewById(R.id.btnTest);
+
+
+       // String[] array = getResources().getStringArray(R.array.randomColours);
+
+
+
+        //How to set a button's background colour to random colour from array.
+        int[] randomColours = getResources().getIntArray(R.array.randomColours);
+        ViewCompat.setBackgroundTintList(btnTest, ColorStateList.valueOf(randomColours[new Random().nextInt(randomColours.length)]));
+
     }
 }
